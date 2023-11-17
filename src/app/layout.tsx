82fa498660be207afa7d1d4f58de09ef28930d86
Thales Cardoso <46228954+RCThales@users.noreply.github.com>
2023-11-17 18:08:51 +0000

    Added Open Graph tags for SEO purposes.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,100 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Thales Cardoso",
   description: "Thales Cardoso's Web Developer Portfolio",
+  openGraph: {
+    title: "Thales Cardoso",
+    description:
+      "Software developer in Canada specialized in react web applications.",
+    url: "https://thalescardoso.dev",
+    siteName: "Thales Cardoso Portfolio",
+    images: [
+      {
+        url: "https://portfolio-bucket-thalescardoso.s3.amazonaws.com/thalesbanner.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://portfolio-bucket-thalescardoso.s3.amazonaws.com/thalesbanner.png",
+        width: 1800,
+        height: 1600,
+        alt: "Thales Cardoso banner.",
+      },
+    ],
+
+    locale: "en_CA",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Software developer in Canada",
+    description:
+      "Software developer in Canada specialized in react web applications.",
+    images: ["https://portfolio-bucket-thalescardoso.s3.amazonaws.com/thalesbanner.png"],
+  },
+  verification: {
+    google: "google",
+    yandex: "yandex",
+    yahoo: "yahoo",
+    other: {
+      me: ["my-email", "my-link"],
+    },
+  },
+  appleWebApp: {
+    title: "Thales' Apple Web App",
+    statusBarStyle: "black-translucent",
+    startupImage: [
+      "https://portfolio-bucket-thalescardoso.s3.amazonaws.com/thalesbanner.png",
+      {
+        url: "https://portfolio-bucket-thalescardoso.s3.amazonaws.com/thalesbanner.png",
+        media: "(device-width: 768px) and (device-height: 1024px)",
+      },
+    ],
+  },
+  category: "development",
+  creator: "Thales Cardoso",
+  keywords: [
+    "Front-end",
+    "front end",
+    "frontend",
+    "developer",
+    "next",
+    "next.js",
+    "react",
+    "react.js",
+    "software",
+    "web",
+    "web developer",
+    "sass",
+    "css",
+    "html",
+    "javascript",
+    "redux",
+    "node",
+    "node.js",
+    "backend",
+    "back-end",
+    "agile",
+    "employee",
+    "dev",
+    "london",
+    "ontario",
+    "canada",
+    "london ontario",
+
+  ],
 };
 
 export default function RootLayout({
